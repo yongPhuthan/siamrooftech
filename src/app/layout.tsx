e2f7204b381090f65c95335044fe4b6d363fa9bc
yongPhuthan <86427357+yongPhuthan@users.noreply.github.com>
2024-10-24 15:25:45 +0700

    test script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,29 +64,18 @@ export default function RootLayout({
       <head>
 
         {/* Google Tag Manager */}
+        
         <Script
-          id="google-tag-manager"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-W6SMLDZG');
-          `,
-          }}
-        />
-         <Script
-        id="google-ads-script"
+        src="https://www.googletagmanager.com/gtag/js?id=AW-11408819333"
         strategy="afterInteractive"
-      >
+        async
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
         {`
-          gtag('event', 'conversion', {
-              'send_to': 'AW-16657093744/6dlECJiE_sYZEPCo3IY-',
-              'value': 1000.0,
-              'currency': 'THB'
-          });
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-11408819333');
         `}
       </Script>
         
@@ -112,6 +101,7 @@ export default function RootLayout({
         </noscript>
         {/* End Google Tag Manager (noscript) */}
         <Providers>
+          
           <Suspense>{children}</Suspense>
         </Providers>
       </body>
