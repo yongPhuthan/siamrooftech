75ccf19047677358dda89adefa2effe5dbe6b462
yongPhuthan <86427357+yongPhuthan@users.noreply.github.com>
2024-10-24 16:51:39 +0700

    rm props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,21 @@
 import Main from './components/Main';
 
 import { Suspense } from 'react'
-interface Props {
-  params: {
-    id: string;
-  };
-  searchParams: { 
-    kw: string;
-  };
-}
+// interface Props {
+//   params: {
+//     id: string;
+//   };
+//   searchParams: { 
+//     kw: string;
+//   };
+// }
 
 
 
-export default async function Home(props: Props) {
+export default async function Home() {
 // const keyword = props.searchParams.kw;
 const keyword  = 'กันสาดพับได้'
   return (
-    
     <div className="bg-white ">
         <Main  keyword={keyword ?  keyword :'กันสาดพับได้'}/>
        {/* <Main projects={projectsByTag} /> */}
