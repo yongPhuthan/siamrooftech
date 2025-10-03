185b92b88c5a550aefc09efde203a2fedbc36d0f
Sukanya Yakam <sukanya@Sukanyas-MacBook-Air.local>
2025-10-03 23:53:25 +0700

    unoptimized: true

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['pub-99f8d7bf688c4c79afcc2d91f37141f2.r2.dev', 'firebasestorage.googleapis.com','siamroof.workstandard.co','assets.siamrooftech.com'],
-        unoptimized: false,
+        domains: ['pub-99f8d7bf688c4c79afcc2d91f37141f2.r2.dev', 'firebasestorage.googleapis.com','siamroof.workstandard.co','assets.siamrooftech.com',],
+        unoptimized: true,
     },
 };
 
